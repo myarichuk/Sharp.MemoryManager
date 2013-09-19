f53b8301c00d77c2cce80bc1bd5b48610d7531b9
Michael Yarichuk <michael.yarichuk@gmail.com>
2013-09-19 21:29:28 +0300

    added misc exception and enum

---
+++ Sharp.MemoryManager/ConcurrencyException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sharp.MemoryManager
+{
+	public class ConcurrencyException : Exception
+	{
+		public ConcurrencyException(string message)
+			: base(message)
+		{
+		}
+
+		public ConcurrencyException(string message,Exception innerException)
+			: base(message,innerException)
+		{
+		}
+	}
+}
---
+++ Sharp.MemoryManager/DataObjects/OperationType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sharp.MemoryManager.DataObjects
+{
+	public enum OperationType
+	{
+		Set,
+		Delete
+	}
+}
